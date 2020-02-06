54b7c4706c96fbbc7a2e656fb1f32d5ec090d32d
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-02-06 15:11:25 -0800

    added controllers/burgers_controller.js, created and exported the router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,10 @@ const express = require("express");
 
 const burger = require("./../models/burger");
 
+const router = express.Router();
+
+router.get("/", function(req, res) {
+
+});
+
+module.exports = router;
